3656abe0a9c0d536ba870eabd0c9f4edf67d4553
Jesper Vallett <smoovefan@hotmail.se>
2018-04-05 16:37:15 +0200

    Calling website for HTML version

--- Assignement 1/index.php
+++ Assignement 1/index.php
@@ -1,5 +1,21 @@
 <?php
+
 require 'vendor/autoload.php';
 
-$res = $client->request('GET', 'http://unicorns.idioti.se/');
+$client = new \GuzzleHttp\Client();
+$header = "Accept: application/json";
+
+$res = $client->request('GET', 'http://unicorns.idioti.se/',  [$header]);
+
+echo $res->getBody();
+
+/*
+if($res->hasHeader('Name')){
+    echo "it exists";
+}
+
+foreach($res->getHeaders() as $name => $values){
+    echo $name . ': ' . implode(', ', $values) . "\r\n";
+} 
+*/
 ?>
